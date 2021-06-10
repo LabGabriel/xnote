907c66fd0d6081dc52639892fddd36b5d43e7f62
Silvério <contato@gabrielsilver.com.br>
2021-06-10 20:32:15 -0300

    styled tabs

---
+++ app/src/Pages/Index/styled.ts
@@ -0,0 +1,41 @@
+import { Tab, TabList } from "react-tabs";
+import styled from "styled-components";
+
+export const TabListStyled = styled(TabList)`
+    display: flex;
+    list-style: none;    
+`
+
+export const TabStyled = styled(Tab)`
+    font-size: 1rem;
+    background-color: black !important;
+    color: white !important;
+    padding: 0.5rem 1.5rem;
+    border-radius: 0.5rem !important;
+    cursor: pointer;
+    border: 0.1rem solid transparent !important;
+    animation: all 0.3s;
+
+    &.react-tabs__tab--selected {
+        border: 0.1rem solid #CD00FE !important;
+    }
+`
+
+export const ButtonPlus = styled.button`
+    font-size: 1rem;
+    border: 0.1rem solid white;    
+    background-color: black;
+    padding: 0.5rem 0.7rem;
+    margin-left: 1rem;
+    border-radius: 0.5rem;
+    cursor: pointer;
+    animation: all 0.3s;
+    color: white;
+
+    :hover {
+        border-color: #CD00FE;                
+    }
+    :active {
+        color: white;
+    }
+`
