6a0a729c54cfb5ad04e9b46f09cc28b12f2609cd
Silvério <contato@gabrielsilver.com.br>
2021-06-10 20:32:09 -0300

    styled tabs

--- app/src/Pages/Index/index.tsx
+++ app/src/Pages/Index/index.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Tabs, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 //Components
 import PageStrecture from 'Components/PageStrecture';
+import TextArea from 'Components/TextArea';
 
-const Index:React.FC = () => {
+//Styled
+import { TabListStyled, TabStyled, ButtonPlus } from './styled';
+
+const Index: React.FC = () => {
+ 
     return (
         <PageStrecture>
-            <p>teste</p>
+            <Tabs>
+                <TabListStyled>
+                    <TabStyled onDoubleClick={changedNameTab}>Title 1</TabStyled>
+                    <ButtonPlus>+</ButtonPlus>
+                </TabListStyled>
+
+                <TabPanel>
+                    <TextArea />
+                </TabPanel>
+            </Tabs>
+
+            
+
+            {/* @typescript-eslint/no-unused-vars */}
+            {/* <Tabs>
+                <TabList>
+                    {obj.map(value => {
+                        return (
+                            <Tab key={value.id}>{value.title}</Tab>
+                        )
+                    })}
+                </TabList>
+
+
+                {obj.map(value => {
+                    return (
+                        <TabPanel>
+                            <h2>{value.content}</h2>
+                        </TabPanel>
+                    )
+                })}
+
+            </Tabs> */}
         </PageStrecture>
     )
 }
