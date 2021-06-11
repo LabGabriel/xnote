2a613034db5349c397ce0d12caed73d6f1255393
Silvério <contato@gabrielsilver.com.br>
2021-06-11 19:29:50 -0300

    indentation

--- app/src/Components/Footer/index.tsx
+++ app/src/Components/Footer/index.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 import { FooterStyled } from './styled';
 
-
 const Footer: React.FC = () => {
     return (
         <FooterStyled>
             <div>
-            <p>Shortcuts</p>
+                <p>Shortcuts</p>
             </div>
         </FooterStyled>
     )
