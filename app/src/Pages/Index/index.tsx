07663066448748f987223a5ba13c5b8df9e40ba2
Silvério <contato@gabrielsilver.com.br>
2021-06-10 21:36:01 -0300

    added components TextArea and InputTabName

--- app/src/Pages/Index/index.tsx
+++ app/src/Pages/Index/index.tsx
@@ -5,47 +5,43 @@ import 'react-tabs/style/react-tabs.css';
 //Components
 import PageStrecture from 'Components/PageStrecture';
 import TextArea from 'Components/TextArea';
+import InputTabName from 'Components/InputTabName';
 
 //Styled
 import { TabListStyled, TabStyled, ButtonPlus } from './styled';
 
 const Index: React.FC = () => {
- 
     return (
         <PageStrecture>
             <Tabs>
                 <TabListStyled>
-                    <TabStyled onDoubleClick={changedNameTab}>Title 1</TabStyled>
-                    <ButtonPlus>+</ButtonPlus>
+                    <div className="tab-area">
+                        <TabStyled>
+                            Tab
+                        </TabStyled>
+                        <TabStyled>
+                            Tab
+                        </TabStyled>
+                        <TabStyled>
+                            Tab
+                        </TabStyled>
+                        <ButtonPlus>+</ButtonPlus>
+                    </div>
+
+                    <InputTabName />
+
                 </TabListStyled>
 
                 <TabPanel>
-                    <TextArea />
+                    <TextArea
+                        defaultValue={""}
+                    />                    
                 </TabPanel>
+                <TabPanel>
+                    2
+                </TabPanel>
+
             </Tabs>
-
-            
-
-            {/* @typescript-eslint/no-unused-vars */}
-            {/* <Tabs>
-                <TabList>
-                    {obj.map(value => {
-                        return (
-                            <Tab key={value.id}>{value.title}</Tab>
-                        )
-                    })}
-                </TabList>
-
-
-                {obj.map(value => {
-                    return (
-                        <TabPanel>
-                            <h2>{value.content}</h2>
-                        </TabPanel>
-                    )
-                })}
-
-            </Tabs> */}
         </PageStrecture>
     )
 }
