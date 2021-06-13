423b0686944ec4420fc3cc26c21390e696db2a71
Silvério <contato@gabrielsilver.com.br>
2021-06-13 13:06:58 -0300

    added button footer

--- app/src/Components/Footer/styled.ts
+++ app/src/Components/Footer/styled.ts
@@ -7,23 +7,42 @@ export const FooterStyled = styled.footer`
     left: 0;
     bottom: 0;
     align-content: center;
+    display: flex;
+    justify-content: space-between;
     
-    /* border: 0.1rem solid white;     */
+    border: 0.1rem solid #121212;
     border-left: 0;
     border-bottom: 0;
     border-right: 0;
     text-align: right;
     background: #000000;
-    background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.3618806989005442) 100%);
 
     div {
-        float: right;
-        p {
-            font-size: 0.8rem;
-            width: min-content;
-            width: -moz-min-content;
-            background-color: #000000;
-            padding: 0.5rem 0.7rem;
+        margin-bottom: 1rem;
+    }
+`
+
+export const NavFooter = styled.ul`
+    list-style: none;
+    font-size: 0.8rem;
+    display: flex;
+
+    li{
+        cursor: pointer;
+        &:first-child {
+            margin-right: 1rem;
+        }
+
+        button {
+            background-color: black;
+            cursor: pointer;
+            border: 0;
+            color: #cfcfcf;
+
+            &:disabled {
+                color: #282525;
+                cursor: not-allowed;
+            }
         }
     }
 `
