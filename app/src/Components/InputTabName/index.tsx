549ba1a5b97f60d9a5840156950f68cf1fe6fff1
Silvério <contato@gabrielsilver.com.br>
2021-06-16 19:11:21 -0300

    changed placeholder

--- app/src/Components/InputTabName/index.tsx
+++ app/src/Components/InputTabName/index.tsx
@@ -11,7 +11,7 @@ const InputTabName:React.FC<InputTabNameTypes> = ({ onChange, onClickClose, onCl
     return (
         <InputArea>            
             <input type="text" name="nameTab" 
-                placeholder="Note name"
+                placeholder="Tab name"
                 autoComplete="off"
                 autoFocus={true}
                 onChange={onChange}
