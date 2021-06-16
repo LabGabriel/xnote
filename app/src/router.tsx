c79f98b6d6827935e2aedde9f6d7eb40c7ad773f
Silvério <contato@gabrielsilver.com.br>
2021-06-16 19:35:02 -0300

    config gh-pages

--- app/src/router.tsx
+++ app/src/router.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 //Pages
 import Index from 'Pages/Index';
-
 
 const Router:React.FC = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={Index}/>
+
+                 {/* Config githubpages */}
+                 <Route path="/xnote">
+                        <Redirect to="/" />
+                    </Route>
+                    <Route path="*">
+                        <Redirect to="/" />
+                    </Route>
             </Switch>
         </BrowserRouter>
     )
