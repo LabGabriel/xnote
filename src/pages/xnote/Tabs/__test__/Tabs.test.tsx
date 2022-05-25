42e1d613b57fbb804455c47be18d3c45933b5f84
Gabriel Silvério <53228013+Gabriel-Silverio-96@users.noreply.github.com>
2022-05-25 19:58:43 -0300

    test(tabs): localStorage (#35)

--- src/pages/xnote/Tabs/__test__/Tabs.test.tsx
+++ src/pages/xnote/Tabs/__test__/Tabs.test.tsx
@@ -3,11 +3,11 @@ import userEvent from "@testing-library/user-event";
 import { XnoteProvider } from "common/context/XnoteContext";
 import createNote from "pages/xnote/common/util/createNote";
 import ReactDOM from "react-dom";
+import localStorageMock from "util/test/localStorageMock";
 import Tabs from "../TabsView";
 
 const MOCK_TAB_TITLE = "Mock title";
 const MOCK_TAB_CONTENT = "My content";
-const MOCK_TAB = [createNote(MOCK_TAB_TITLE)];
 
 const TabRender: React.FC<any> = ({ storage = [], ...rest }) => {
     return (
@@ -17,7 +17,10 @@ const TabRender: React.FC<any> = ({ storage = [], ...rest }) => {
     )
 }
 
+beforeEach(() => localStorageMock());
 afterEach(cleanup);
+afterAll(jest.clearAllMocks);
+
 describe("Test component <Tab />", () => {
     it("Should render the component", () => {
         const { container } = render(<TabRender />);
@@ -28,39 +31,56 @@ describe("Test component <Tab />", () => {
         const div = document.createElement("div");
         ReactDOM.render(<TabRender />, div);
         ReactDOM.unmountComponentAtNode(div);
-    });    
+    });
 
     it("Should show one tab", () => {
-        const { getByText } = render(<TabRender storage={MOCK_TAB} />);
-        const tab = getByText(MOCK_TAB.shift()?.title!);
-    
+        const { getByText } = render(<TabRender storage={[createNote(MOCK_TAB_TITLE)]} />);
+        const tab = getByText(MOCK_TAB_TITLE);
+
         expect(tab).toBeInTheDocument();
     });
-    
+
     it("Should have one called click button openDialogCreate", () => {
         const openDialogCreate = jest.fn();
         const { getByText } = render(<TabRender {...{ openDialogCreate }} />);
         const buttonPlus = getByText("+") as HTMLButtonElement;
         userEvent.click(buttonPlus);
-    
+
         expect(openDialogCreate).toHaveBeenCalledTimes(1);
     });
-    
+
     it("Should have one called click button openDialogEdit", () => {
         const openDialogEdit = jest.fn();
-        const { getByText } = render(<TabRender {...{ openDialogEdit }} storage={MOCK_TAB} />);
-        const tab = getByText(MOCK_TAB.shift()?.title!) as HTMLLIElement;
+        const { getByText } = render(<TabRender {...{ openDialogEdit }} storage={[createNote(MOCK_TAB_TITLE)]} />);
+        const tab = getByText(MOCK_TAB_TITLE) as HTMLLIElement;
         userEvent.dblClick(tab);
-    
+
         expect(openDialogEdit).toHaveBeenCalledTimes(1);
     });
-    
+
     it(`Should have ${MOCK_TAB_CONTENT.length} called onChange textarea handleContent`, () => {
         const handleContent = jest.fn();
-        const { container } = render(<TabRender {...{ handleContent }} storage={MOCK_TAB} />);
+        const { container } = render(<TabRender {...{ handleContent }} storage={[createNote(MOCK_TAB_TITLE)]} />);
         const textArea = container.querySelector("textarea") as HTMLTextAreaElement;
         userEvent.type(textArea, MOCK_TAB_CONTENT);
-    
+
         expect(handleContent).toHaveBeenCalledTimes(MOCK_TAB_CONTENT.length);
-    });      
+    });
+
+    it("Should have one called localStorage setItem and getItem", () => {
+        const { container } = render(<TabRender storage={[createNote(MOCK_TAB_TITLE)]} />);
+        const textArea = container.querySelector("textarea") as HTMLTextAreaElement;
+        userEvent.type(textArea, MOCK_TAB_CONTENT);
+
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+    });
+
+    it("Should show localStorage getItem xnote", () => {
+        const { container } = render(<TabRender storage={[createNote(MOCK_TAB_TITLE)]} />);
+        const textArea = container.querySelector("textarea") as HTMLTextAreaElement;
+        userEvent.type(textArea, MOCK_TAB_CONTENT);
+
+        expect(localStorage.getItem).toHaveBeenCalledWith("xnote");
+    });
 });
