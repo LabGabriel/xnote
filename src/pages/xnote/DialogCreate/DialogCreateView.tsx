d5a8b77daced72cdb8bb7631b94a7d2a877f29c0
Gabriel Silvério <53228013+Gabriel-Silverio-96@users.noreply.github.com>
2022-05-27 18:35:40 -0300

    refactor(dialog-create): change prop open (#41)

--- src/pages/xnote/DialogCreate/DialogCreateView.tsx
+++ src/pages/xnote/DialogCreate/DialogCreateView.tsx
@@ -5,34 +5,38 @@ import { ButtonClose, ButtonPrimary, ButtonSecundary, Dialog, DialogBody, Dialog
 const DialogCreateView: React.FC<IDialogCreate> = ({ open, register, errors, handleSubmit, onSubmit, onClose }) => {
     const isErrors = errors.title?.type === "required";
     return (
-        <Dialog role="dialog" open={open}>
-            <DialogContent>
-                <DialogHeader>
-                    <h3>Create new note</h3>
-                    <ButtonClose onClick={onClose}>
-                        <p>+</p>
-                    </ButtonClose>
-                </DialogHeader>
-                <DialogBody>
-                    <form onSubmit={handleSubmit(onSubmit)} id="createNote">
-                        <Input
-                            {
-                            ...register("title",
-                                { required: true })
-                            }
-                            placeholder="Tab name"
-                            data-testid="create-title"
-                        />
-                        {isErrors && <span>Tab name is required</span>}
-                    </form>
-                </DialogBody>
+        <>
+            {open && (
+                <Dialog role="dialog">
+                    <DialogContent>
+                        <DialogHeader>
+                            <h3>Create new note</h3>
+                            <ButtonClose onClick={onClose}>
+                                <p>+</p>
+                            </ButtonClose>
+                        </DialogHeader>
+                        <DialogBody>
+                            <form onSubmit={handleSubmit(onSubmit)} id="createNote">
+                                <Input
+                                    {
+                                    ...register("title",
+                                        { required: true })
+                                    }
+                                    placeholder="Tab name"
+                                    data-testid="create-title"
+                                />
+                                {isErrors && <span>Tab name is required</span>}
+                            </form>
+                        </DialogBody>
 
-                <DialogFooter>
-                    <ButtonSecundary onClick={onClose}>Close</ButtonSecundary>
-                    <ButtonPrimary form="createNote" data-testid="save">Save</ButtonPrimary>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
+                        <DialogFooter>
+                            <ButtonSecundary onClick={onClose}>Close</ButtonSecundary>
+                            <ButtonPrimary form="createNote" data-testid="save">Save</ButtonPrimary>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
+            )}
+        </>
     )
 };
 
