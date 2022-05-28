dc11cb3e6b0a626045b732102d41cd106fe4f9d7
Gabriel Silvério <53228013+Gabriel-Silverio-96@users.noreply.github.com>
2022-05-28 19:38:53 -0300

    test(dialog-create): submit form (#43)

--- src/pages/xnote/DialogCreate/__test__/DialogCreate.test.tsx
+++ src/pages/xnote/DialogCreate/__test__/DialogCreate.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, render, waitFor } from "@testing-library/react";
+import { cleanup, fireEvent, render, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { XnoteProvider } from "common/context/XnoteContext";
 import { INoteFields } from "pages/xnote/common/types/dialog";
@@ -11,7 +11,7 @@ const TITLE_DIALOG = "Create new note";
 const MOCK_TITLE_NOTE = "New note";
 const MOCK_MESSAGE_ERROR = "Tab name is required";
 
-const DialogCreateRender: React.FC<any> = (props) => {
+const DialogCreateRender: React.FC<any> = ({ submitMock }) => {
     const [open, setOpen] = useState<boolean>(false);
     const { register, formState: { errors }, handleSubmit, reset } = useForm<INoteFields>();
 
@@ -21,7 +21,10 @@ const DialogCreateRender: React.FC<any> = (props) => {
 
     return (
         <XnoteProvider>
-            <DialogCreate {...props} {...{ register, errors, handleSubmit, onSubmit, reset, onClose, open }} />
+            <DialogCreate
+                handleSubmit={submitMock ? submitMock : handleSubmit}
+                {...{ register, errors, onSubmit, reset, onClose, open }}
+            />
             <button onClick={onOpen} data-testid="button-open-dialog">Open dialog</button>
         </XnoteProvider>
     )
@@ -38,43 +41,57 @@ describe("Test component <DialogCreate />", () => {
         const div = document.createElement("div");
         ReactDOM.render(<DialogCreateRender />, div);
         ReactDOM.unmountComponentAtNode(div);
-    });    
+    });
 
-    it("Should show dialog display none when open equal false", () => {
-        const { queryByText } = render(<DialogCreateRender />);        
-        
+    it(`Should not show ${TITLE_DIALOG} when dialog is close`, () => {
+        const { queryByText } = render(<DialogCreateRender />);
+
         expect(queryByText(TITLE_DIALOG)).not.toBeInTheDocument();
     });
-    
-    it("Should show dialog display block when open equal true", () => {
-        const { queryByText, getByTestId } = render(<DialogCreateRender />);        
+
+    it(`Should show ${TITLE_DIALOG} when dialog is open`, () => {
+        const { queryByText, getByTestId } = render(<DialogCreateRender />);
         const buttonOpenDialog = getByTestId("button-open-dialog") as HTMLButtonElement;
         userEvent.click(buttonOpenDialog);
-    
-        expect(queryByText(TITLE_DIALOG)).toBeInTheDocument();        
+
+        expect(queryByText(TITLE_DIALOG)).toBeInTheDocument();
     });
-    
+
     it(`Should show message error ${MOCK_MESSAGE_ERROR} when input title value empty`, async () => {
         const { getByTestId, getByText } = render(<DialogCreateRender />);
         const buttonOpenDialog = getByTestId("button-open-dialog") as HTMLButtonElement;
         userEvent.click(buttonOpenDialog);
-    
+
         await waitFor(() => {
             const buttonSave = getByText(/save/i) as HTMLButtonElement;
             userEvent.click(buttonSave);
         })
-        
+
         expect(getByText(MOCK_MESSAGE_ERROR)).toBeInTheDocument();
     });
-    
+
     it(`Should show input title value ${MOCK_TITLE_NOTE}`, () => {
         const { container, getByTestId } = render(<DialogCreateRender />);
         const buttonOpenDialog = getByTestId("button-open-dialog") as HTMLButtonElement;
         userEvent.click(buttonOpenDialog);
-    
+
         const inputTitle = container.querySelector("[name=title]") as HTMLInputElement;
         userEvent.type(inputTitle, MOCK_TITLE_NOTE);
-    
+
         expect(inputTitle).toHaveValue(MOCK_TITLE_NOTE);
     });
+
+    it("Should show one called handleSubmit when submit", async () => {
+        const handleSubmit = jest.fn();
+        const { getByTestId, container } = render(<DialogCreateRender submitMock={handleSubmit} />);
+
+        const buttonOpenDialog = getByTestId("button-open-dialog") as HTMLButtonElement;
+        userEvent.click(buttonOpenDialog);
+
+        const inputTitle = container.querySelector("[name=title]") as HTMLInputElement;
+        userEvent.type(inputTitle, MOCK_TITLE_NOTE);
+
+        fireEvent.submit(getByTestId("save"));        
+        expect(handleSubmit).toHaveBeenCalledTimes(1);
+    });  
 });
